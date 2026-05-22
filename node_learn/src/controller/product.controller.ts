7ddb5e3c90f0fceccf17c96338618838916adcd0
MRM Abdullah <mrmabdullah2024@gmail.com>
2026-05-21 23:21:38 -0400

    v-6 postman bebohar

--- node_learn/src/controller/product.controller.ts
+++ node_learn/src/controller/product.controller.ts
@@ -1,6 +1,7 @@
 import type { IncomingMessage, ServerResponse } from "http";
 import { readProduct } from "../service/product.service";
 import type { IProduct } from "../types/product.type";
+import { parseBody } from "../utility/parseBody";
 
 export const productController = async(req: IncomingMessage, res: ServerResponse) => {
     const products = readProduct()
@@ -9,10 +10,10 @@ export const productController = async(req: IncomingMessage, res: ServerResponse
 //   console.log(urlParts);
     const method = req.method
 
-    const id = urlParts && urlParts[1] === "product" ? Number(urlParts[2]) : null;
+    const id = urlParts && urlParts[1] === "products" ? Number(urlParts[2]) : null;
     // console.log(id)
 
-    if (url === '/product' && method === "GET"){
+    if (url === '/products' && method === "GET"){
         res.writeHead(200, {"content-type": "application/json"})
         res.end(JSON.stringify({Message:   "this is Products page", data : products}))
         
@@ -21,7 +22,20 @@ export const productController = async(req: IncomingMessage, res: ServerResponse
     else if (method === "GET" && id !== null)  {
       const products = readProduct();
       const product = products.find((p: IProduct) => p.id === id);
-      res.end(JSON.stringify({Message:   "this is Product page", data : product}))
+      res.end(JSON.stringify({Message:  "this is Product page", data : product}))
+    
+    }else if (method === "POST" && url === "/products") {
+    // Created Product by Post Method
+    const body = await parseBody(req);
+    console.log("Body", body);
+    res.writeHead(200, {"content-type": "application/json"})
+    res.end(JSON.stringify({Message: "product created"}))
+
+    // const products = readProduct(); // [{},{},{}]
+    // const newProduct = {
+    //   id: Date.now(),
+    //   ...body,
+    // };
     }
     
 };
