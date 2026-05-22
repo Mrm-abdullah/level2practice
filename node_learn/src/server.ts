3efc74158fef4d2a9d4563ba62054836e84f499b
MRM Abdullah <mrmabdullah2024@gmail.com>
2026-05-21 20:01:16 -0400

    v-2 http req & res

--- node_learn/src/server.ts
+++ node_learn/src/server.ts
@@ -1,10 +1,27 @@
+
 import console from "console"
 import {createServer, Server, IncomingMessage, ServerResponse} from "http"
-import { routeHandler } from "./routes/route"
 
 const server : Server = createServer((req: IncomingMessage, res: ServerResponse)=>{
     // console.log(req.url);
-    routeHandler(req, res);
+    // console.log(req.method);
+    const url = req.url
+    const method = req.method
+    if (url === '/' && method === "GET") {
+        // res.writeHead(200, {"content-type": "text/plain"})
+        // res.end("this is hone page")
+
+        res.writeHead(200, {"content-type": "application/json"})
+        res.end(JSON.stringify({Message:   "this is hone page"})) 
+    }else if (url?.startsWith("/product")) {
+        res.writeHead(200, {"content-type": "application/json"})
+        res.end(JSON.stringify({Message:   "this is Product page"}))
+    }
+    
+    else{
+        res.writeHead(404, {"content-type": "text/plain"})
+        res.end("Content not found")
+    }
 })
 
 server.listen(5000,()=>{
