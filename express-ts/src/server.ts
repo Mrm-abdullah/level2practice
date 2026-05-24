6529e17f8268969167a6df789fc31772f6dce98a
MRM Abdullah <mrmabdullah2024@gmail.com>
2026-05-24 05:35:22 -0400

    v-7.8-9 user update and delete

--- express-ts/src/server.ts
+++ express-ts/src/server.ts
@@ -90,87 +90,82 @@ app.get("/api/users/:id", async (req: Request, res: Response) => {
   }
 });
 
-// app.put("/api/users/:id", async (req: Request, res: Response) => {
-//   const { id } = req.params;
-//   const { name, password, age, is_active } = req.body;
+app.put("/api/users/:id", async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const { name, password, age, is_active } = req.body;
 
-//   // console.log("Id : ", id);
-//   // console.log({ name, password, age, is_active });
+  // console.log("Id : ", id);
+  // console.log({ name, password, age, is_active });
 
-//   try {
-//     const result = await pool.query(
-//       `
-//     UPDATE users 
-//     SET 
-//     name=COALESCE($1,name),
-//     password=COALESCE($2,password),
-//     age=COALESCE($3,age),
-//     is_active=COALESCE($4,is_active) 
+  try {
+    const result = await pool.query(
+      `
+    UPDATE users 
+    SET 
+    name=COALESCE($1,name),
+    password=COALESCE($2,password),
+    age=COALESCE($3,age),
+    is_active=COALESCE($4,is_active) 
 
-//     WHERE id=$5 RETURNING *
-//     `,
-//       [name, password, age, is_active, id],
-//     );
+    WHERE id=$5 RETURNING *
+    `,
+      [name, password, age, is_active, id],
+    );
 
-//     if (result.rows.length === 0) {
-//       res.status(404).json({
-//         success: false,
-//         message: "User Not found!",
-//       });
-//     }
+    if (result.rows.length === 0) {
+      res.status(404).json({
+        success: false,
+        message: "User Not found!",
+      });
+    }
 
-//     // console.log(result);
-//     res.status(200).json({
-//       success: true,
-//       message: "User updated successfully!",
-//       data: result.rows[0],
-//     });
-//   } catch (error: any) {
-//     res.status(500).json({
-//       success: false,
-//       message: error.message,
-//       error: error,
-//     });
-//   }
-// });
+    // console.log(result);
+    res.status(200).json({
+      success: true,
+      message: "User updated successfully!",
+      data: result.rows[0],
+    });
+  } catch (error: any) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+      error: error,
+    });
+  }
+});
 
-// app.delete("/api/users/:id", async (req: Request, res: Response) => {
-//   const { id } = req.params;
-//   try {
-//     const result = await pool.query(
-//       `
-//     DELETE FROM users WHERE id=$1  
-//       `,
-//       [id],
-//     );
+app.delete("/api/users/:id", async (req: Request, res: Response) => {
+  const { id } = req.params;
+  try {
+    const result = await pool.query(
+      `
+    DELETE FROM users WHERE id=$1  
+      `,
+      [id],
+    );
 
-//     console.log(result);
-//     if (result.rowCount === 0) {
-//       res.status(404).json({
-//         success: false,
-//         message: "User Not found!",
-//       });
-//     }
+    console.log(result);
+    if (result.rowCount === 0) {
+      res.status(404).json({
+        success: false,
+        message: "User Not found!",
+      });
+    }
 
-//     res.status(200).json({
-//       success: true,
-//       message: "User deleted successfully!",
-//       data: {},
-//     });
-//   } catch (error: any) {
-//     res.status(500).json({
-//       success: false,
-//       message: error.message,
-//       error: error,
-//     });
-//   }
-// });
-
-// app.listen(port, () => {
-//   console.log(`Example app listening on port ${port}`);
-// });
-
+    res.status(200).json({
+      success: true,
+      message: "User deleted successfully!",
+      data: {},
+    });
+  } catch (error: any) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+      error: error,
+    });
+  }
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
